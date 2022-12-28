ae6e480318add58913fdfe395b4ae981779302ae
huhuang03 <huhuang03@gmail.com>
2022-12-28 20:31:38 +0800

    feat: add e_shstrndx

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,63 @@
 #include <reader_util.h>
 #include <fstream>
 
+namespace elf_reader {
+#define EI_NIDENT 16
+typedef uint16_t Elf32_Half;
+typedef int16_t Elf32_SHalf;
+typedef uint32_t Elf32_Word;
+typedef int32_t Elf32_Sword;
+typedef uint64_t Elf32_Xword;
+typedef int64_t Elf32_Sxword;
+
+typedef uint32_t Elf32_Off;
+typedef uint32_t Elf32_Addr;
+typedef uint16_t Elf32_Section;
+
+typedef struct {
+  unsigned char e_ident[EI_NIDENT];
+  Elf32_Half e_type;
+  Elf32_Half e_machine;
+  Elf32_Word e_version;
+  Elf32_Addr e_entry;
+  Elf32_Off e_phoff;
+  Elf32_Off e_shoff;
+  Elf32_Word e_flags;
+  Elf32_Half e_ehsize;
+  Elf32_Half e_phentsize;
+  Elf32_Half e_phnum;
+  Elf32_Half e_shentsize;
+  Elf32_Half e_shnum;
+  Elf32_Half e_shstrndx;
+} Elf32_Ehdr;
+
+typedef uint64_t	Elf64_Addr;
+typedef uint16_t	Elf64_Half;
+typedef uint64_t	Elf64_Off;
+typedef int32_t		Elf64_Sword;
+typedef int64_t		Elf64_Sxword;
+typedef uint32_t	Elf64_Word;
+typedef uint64_t	Elf64_Lword;
+typedef uint64_t	Elf64_Xword;
+
+typedef struct {
+  unsigned char e_ident[EI_NIDENT];
+  Elf64_Half e_type;
+  Elf64_Half e_machine;
+  Elf64_Word e_version;
+  Elf64_Addr e_entry;
+  Elf64_Off e_phoff;
+  Elf64_Off e_shoff;
+  Elf64_Word e_flags;
+  Elf64_Half e_ehsize;
+  Elf64_Half e_phentsize;
+  Elf64_Half e_phnum;
+  Elf64_Half e_shentsize;
+  Elf64_Half e_shnum;
+  Elf64_Half e_shstrndx;
+} Elf64_Ehdr;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     std::cout << "Usage: " << argv[0] << " filepath" << std::endl;
@@ -15,6 +72,14 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // how to do this?
+
   std::ifstream f;
   f.open(argv[1]);
+
+  elf_reader::Elf64_Ehdr ehdr;
+  f.read(reinterpret_cast<char *>(&ehdr), sizeof ehdr);
+  // how to do this?
+  std::cout << "e_shnum: " << ehdr.e_shnum << std::endl;
+  std::cout << "e_shstrndx: " << ehdr.e_shstrndx << std::endl;
 }
